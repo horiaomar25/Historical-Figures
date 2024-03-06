491c1a59dd6488150e77739e9b33da404575907f
horiaomar25 <horiaomar6@gmail.com>
2024-03-06 16:10:21 +0000

    layout added for search

--- british-historical-figures/app/page.js
+++ british-historical-figures/app/page.js
@@ -4,6 +4,12 @@ import WikiData from "@/API/WikiData";
 
 export default function Home() {
   return(
-  <WikiData />
+    <>
+    <div className="flex flex-col justify-center items-center m-8">
+    <h1 className="text-white text-4xl text-center font-bold border border-white mb-10">Historical Figures</h1> 
+    <input type="text" className="w-1/2 border border-black h-10 rounded-lg"/>
+    </div>
+   
+    </>
   ) 
 }
