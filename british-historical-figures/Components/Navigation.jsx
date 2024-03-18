4e99f01ccbccf680271045bd28b9bcd1a73cbe66
horiaomar25 <horiaomar6@gmail.com>
2024-03-18 16:11:56 +0000

    toggle menu has been completed

--- british-historical-figures/Components/Navigation.jsx
+++ british-historical-figures/Components/Navigation.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Image from 'next/image'
 
 
 const Navigation = () => {
+    // set State to toggle menu
+    const[isOpen, setIsOpen] = useState(false);
+
+    // function to toggle menu
+    const toggleMenu =  () => {
+        setIsOpen(!isOpen);
+    }
   return (
-    <header className='w-1/5 border border-white h-screen'>
+    <>
+    { !isOpen && <button onClick={toggleMenu} className='m-8 '><img src='/menu.png' className='w-10'/></button>}
+    {isOpen && (
+    <header className='w-1/2 border border-white h-screen md:w-1/5 '>     
+    <Image src={"/cross.png"} width={30} height={30} alt="cross-icon" className='float-right m-4' onClick={toggleMenu}/>
         <nav>
-            <Image src={"/cross.png"} width={30} height={30} alt="cross-icon" className='float-right m-4'/>
-            <ul className='text-white text-center p-4 m-4'>
-                
-                <li>Home</li>
+       
+            <ul className='text-white flex flex-col items-center justify-center p-4 '>
+                <li className='mt-8'>Home</li>
                 <li className='mt-4'>Search</li>
             </ul>
 
         </nav>
 
     </header>
+    )}
+    </>
   )
 }
 
