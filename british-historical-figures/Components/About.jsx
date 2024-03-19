0a71e3204720171482a9f838d4d8bad5197b4681
horiaomar25 <horiaomar6@gmail.com>
2024-03-19 17:34:16 +0000

    styling added to about section

--- british-historical-figures/Components/About.jsx
+++ british-historical-figures/Components/About.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import WikiData from '@/API/WikiData';
+import Image from 'next/image';
 
 const About = () => {
-  // Array of historical figures to fetch data for
-  const historicalFigures = ["Martin Luther King Jr", "Marie Curie", "William Shakespeare", "Ada Lovelace"];
-
+ 
   return (
-    <div className='h-screen '>
-      <h2 className='text-4xl '>Learn about Important Figures in History</h2>
-      {/* Iterate over the historicalFigures array and render WikiData component */}
-      {historicalFigures.map((figure, index) => (
-        <div key={index} >
-          <WikiData searchQuery={figure} />
+    <section className='h-screen '>
+      
+      <div className="flex  flex-col md:flex-row justify-center items-center ">
+        <div className='flex flex-col '> 
+      <Image src={"http://localhost:3000/_next/image?url=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fc%2Fc8%2FMarie_Curie_c._1920s.jpg%2F300px-Marie_Curie_c._1920s.jpg&w=640&q=75"} width={300} height={200} className='rounded-lg mb-4 mt-8 ' alt="Marie Curie"/>
+      <Image src={"http://localhost:3000/_next/image?url=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F0%2F05%2FMartin_Luther_King%252C_Jr..jpg%2F300px-Martin_Luther_King%252C_Jr..jpg&w=640&q=75"} width={300} height={175} className='rounded-lg ' alt="Martin Luther King"/>
         </div>
-      ))}
-    </div>
+        <h2 className='text-4xl text-center p-10 '>Learn about Important Figures in History</h2>
+      <div className='flex flex-col '>
+      <Image src={"http://localhost:3000/_next/image?url=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F5%2F5e%2FWilliam_Shakespeare_by_John_Taylor.jpg%2F300px-William_Shakespeare_by_John_Taylor.jpg&w=640&q=75"} width={300} height={200} className='rounded-lg mb-4' alt="William Shakespeare"/>
+      <Image src={"http://localhost:3000/_next/image?url=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fb%2Fbc%2FSir_Winston_Churchill_-_19086236948.jpg%2F300px-Sir_Winston_Churchill_-_19086236948.jpg&w=640&q=75"} width={300} height={200} className='rounded-lg ' alt="Ada Lovelace"/>
+      </div>
+</div>
+    </section>
   );
 };
 
