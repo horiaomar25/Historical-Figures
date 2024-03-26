ba691344b93618403739817be7c2ac4e08b21417
horiaomar25 <horiaomar6@gmail.com>
2024-03-26 13:35:40 +0000

    added: changes to the about section

--- british-historical-figures/Components/About.jsx
+++ british-historical-figures/Components/About.jsx
@@ -1,20 +1,18 @@
-import React, { useEffect, useRef } from "react";
-import Image from "next/image";
+import React from "react";
 
 const About = () => {
   return (
-    <>
-      <section className="h-screen mt-10 border border-black m-6">
-        <div className="flex flex-col justify-center items-center">
-          <h1>Quiz</h1>
-          <p>Test your knowledge with this quiz</p>
-          <button className="border border-black w-1/5 rounded-lg p-2 bg-dark-brown">
-            Quiz
-          </button>
-        </div>
-      </section>
-    </>
+    <section className="h-screen mt-10  m-6 flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center ">
+        <h1>Quiz</h1>
+        <p>Test your knowledge with this quiz</p>
+        <button className="border border-black w-1/2 rounded-lg p-2 bg-dark-brown">
+          Quiz
+        </button>
+      </div>
+    </section>
   );
 };
 
 export default About;
+
