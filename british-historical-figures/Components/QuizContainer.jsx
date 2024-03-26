f2eef703eb24fe38f7e67a1ca19473067d1df412
horiaomar25 <horiaomar6@gmail.com>
2024-03-26 13:16:23 +0000

    added: quiz changed

--- british-historical-figures/Components/QuizContainer.jsx
+++ british-historical-figures/Components/QuizContainer.jsx
@@ -1,10 +1,11 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react'
 
 const QuizContainer = () => {
   return (
     <section className='flex justify-center items-center'>
       <div className='border border-black w-1/2 h-96'>
-
+       <h2 className='text-center mt-2 text-2xl'>Who is often referred to as the "Iron Lady"?</h2>
       </div>
     </section>
   )
