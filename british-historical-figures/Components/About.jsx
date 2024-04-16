9c9c058940b871b37b782a43e767af1272d9c7b9
horiaomar25 <horiaomar6@gmail.com>
2024-04-16 17:58:35 +0100

    added: new update

--- british-historical-figures/Components/About.jsx
+++ british-historical-figures/Components/About.jsx
@@ -5,50 +5,43 @@ const About = () => {
   return (
     <section className="h-screen flex flex-col justify-center items-center">
       <div className="flex flex-col justify-center items-center ">
-
-        <h3 className="text-white text-7xl text-center font-bold font-Inter ">Try this <span className="pb-4 font-extrabold tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-cyan-500 to-blue-500">Quiz</span></h3>
-        <p className="font-Inter text-white mt-4">Test your knowledge with this quiz</p>
-        <button className="border border-black w-1/2 rounded-lg p-2 ">
-          
-        </button>
+        <h3 className="text-white text-7xl text-center font-bold font-Inter ">
+          Try this{" "}
+          <span className="pb-4 font-extrabold tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-cyan-500 to-blue-500">
+            Quiz
+          </span>
+        </h3>
+        <p className="font-Inter text-white mt-4">
+          Test your knowledge with this quiz
+        </p>
+        <button className="border border-black w-1/2 rounded-lg p-2 "></button>
       </div>
 
-      
-      
       {/* This part will be about the quiz build a blank quiz square */}
-      <div className=" w-full md:w-1/2 flex justify-center items-center flex-col ">
-         <div className="w-10/12 h-96 bg-gray-500 border border-black rounded-xl bg-opacity-50 backdrop-filter-blur border-opacity-25" >
-          <div className="text-white font-Inter text-xl text-center mt-4">
-            <p>Question...</p>
-          </div>
+      <div className=" w-full  md:w-11/12 flex justify-center items-center flex-col md:flex-row md:justify-between ">
+        {/* Card in the about section holding the quiz*/}
+        <div className="w-1/2 h-96 bg-gray-500 border border-black rounded-xl bg-opacity-50 backdrop-filter-blur border-opacity-25">
+          <p className="text-white font-Inter text-xl text-center mt-4">
+            Question...
+          </p>
 
-          <div className="border border-black w-full">
-          <div className="border border-white w-auto p-5 m-8 rounded-2xl">
+          <div className="border border-white w-auto p-5 m-8 rounded-2xl"></div>
 
-          </div>
+          <div className="border border-white w-auto p-5 m-8 rounded-2xl"></div>
 
-          <div className="border border-white w-auto p-5 m-8 rounded-2xl">
+          <div className="border border-white w-auto p-5 m-8 rounded-2xl bg-green-500"></div>
 
-          </div>
+          <div className="border border-white w-auto p-5 m-8 rounded-2xl"></div>
+        </div>
 
-          <div className="border border-white w-auto p-5 m-8 rounded-2xl bg-green-500">
-
-          </div>
-
-          <div className="border border-white w-auto p-5 m-8 rounded-2xl">
-
-          </div>
-</div>
-         </div>
-
-         <div>
-          <p className="text-white font-Inter">Hello my name is </p>
-         </div>
-</div>
-
+       
+        <div className="text-center flex justify-center mt-6 item-center border border-white w-full md:w-1/2"> 
+       <p className="text-white font-Inter text-center">Hello my name is </p>
+       </div> 
+      </div>
+      
     </section>
   );
 };
 
 export default About;
-
